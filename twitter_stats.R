32bcd1e538468e8de9fdd5ae77b7dcbb795409e1
laurin-kraemer <65118747+laurin-kraemer@users.noreply.github.com>
2022-02-07 22:02:22 -0800

    Updated Authentification & statistics

--- twitter_stats.R
+++ twitter_stats.R
@@ -15,14 +15,24 @@ library(rtweet)
 
 # Authentificate Twitter
 
+consumer_key <- Sys.getenv("TWITTER_CONSUMER_API_KEY")
+consumer_secret <- Sys.getenv("TWITTER_CONSUMER_API_SECRET")
+access_token <- Sys.getenv("TWITTER_ACCESS_TOKEN")
+access_secret <- Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+# Authenticate Token for rtweet
 twitter_token <- rtweet::create_token(
   app = "JustinDefi",
-  consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
-  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
-  access_token <- Sys.getenv("TWITTER_ACCESS_TOKEN"),
-  access_secret <- Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+  consumer_key = consumer_key,
+  consumer_secret = consumer_secret,
+  access_token <- access_token ,
+  access_secret <- access_secret,
   set_renv = TRUE)
 
+# Authenticate twittR
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+
+# Select collection of interest
 collection <- 'boredapeyachtclub'
 
 # Retrieving specific data about single collection
@@ -34,10 +44,26 @@ stats <- content$collection$stats
 contract <- content$collection$primary_asset_contracts
 
 # Get description
-description <- content$description
+description <- content$collection$primary_asset_contracts[[1]]$description
+
+# Token Standard
+token_standard <- content$collection$primary_asset_contracts[[1]]$schema_name
+
+# Date created
+date_created <- content$collection$primary_asset_contracts[[1]]$created_date
+date_created <- as.POSIXct(date_created, format="%Y-%m-%dT%H:%M:%S", tz="UTC")
+
+# Extract Year/Month/Date
+date_created <- strftime(date_created, format="%Y-%m-%d")
 
 # Find out Twitter user
-bored_ape_twitter <- content$collection$twitter_username
+twitter_user <- content$collection$twitter_username
+
+# Collection Size
+n_pieces <- stats$total_supply
+
+# Number of Owners
+n_owners <- stats$num_owners
 
 # Floor Price
 floor_price <- stats$floor_price
@@ -62,6 +88,15 @@ avg_30_days <- format(round(stats$thirty_day_average_price, 3), nsmall = 3)
 
 # Market Cap
 market_cap <- format(round(stats$market_cap, 1), nsmall = 0) 
+
+# Get Twitter User Object
+user <- getUser(twitter_user)
+
+# Followers count
+n_follower <- followersCount(user)
+
+# Is the user verified?
+verified <- verified(user)
 
 
 
