088175169b4c285ab017a5250a764b65fb32b06e
laurin-kraemer <65118747+laurin-kraemer@users.noreply.github.com>
2022-02-07 21:02:35 -0800

    Update twitter_stats.R

--- twitter_stats.R
+++ twitter_stats.R
@@ -1,3 +1,4 @@
+# Import libraries
 library(httr)
 library(usethis)
 library(jsonlite)
@@ -12,6 +13,8 @@ library(jsonlite)
 library(rjson)
 library(rtweet)
 
+# Authentificate Twitter
+
 twitter_token <- rtweet::create_token(
   app = "JustinDefi",
   consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
@@ -19,3 +22,46 @@ twitter_token <- rtweet::create_token(
   access_token <- Sys.getenv("TWITTER_ACCESS_TOKEN"),
   access_secret <- Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
   set_renv = TRUE)
+
+collection <- 'boredapeyachtclub'
+
+# Retrieving specific data about single collection
+r <- GET(paste("https://api.opensea.io/api/v1/collection/",collection, sep=''))
+content <- fromJSON(prettify(r, indent = 4))
+
+# Get Stats and contract details about Collection
+stats <- content$collection$stats
+contract <- content$collection$primary_asset_contracts
+
+# Get description
+description <- content$description
+
+# Find out Twitter user
+bored_ape_twitter <- content$collection$twitter_username
+
+# Floor Price
+floor_price <- stats$floor_price
+
+# Sales - One day
+n_day_sales <- stats$one_day_sales
+
+# Average price - One day 
+day_avg_price <- format(round(stats$one_day_average_price, 3), nsmall = 3) 
+
+# Sales - seven days
+n_week_sales <- stats$seven_day_sales
+
+# Average price - seven days
+week_avg_price <- format(round(stats$seven_day_average_price, 3), nsmall = 3) 
+  
+# Sales - Last 30 Days
+n_month_sales <- stats$thirty_day_sales
+
+# Average Price - Last 30 Days
+avg_30_days <- format(round(stats$thirty_day_average_price, 3), nsmall = 3) 
+
+# Market Cap
+market_cap <- format(round(stats$market_cap, 1), nsmall = 0) 
+
+
+
